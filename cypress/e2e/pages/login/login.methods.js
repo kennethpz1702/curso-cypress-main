1082fd021eb1dc18aa5dfd41122b3d3ef8bd3aeb
kennethpz1702 <kennethpz1702@gmail.com>
2025-05-12 18:43:52 -0600

    Fix reports and flakyness

--- cypress/e2e/pages/login/login.methods.js
+++ cypress/e2e/pages/login/login.methods.js
@@ -2,14 +2,14 @@ import { LoginElements } from "./login.elements";
 
 export class LoginMethods{
     static insertUsername(username){
-        LoginElements.textboxes.username.type(username) //Acá le decimos que vaya a la clase LoginElements, busque un método
+        LoginElements.textboxes.username.invoke('val',username) //Acá le decimos que vaya a la clase LoginElements, busque un método
                                                         //que se llame textboxes y busque el elemento de username.
                                                         //type es un método de cypress para insertar y se le pasa el username como 
                                                         //propiedad
     }
 
     static insertPassword(password){
-        LoginElements.textboxes.password.type(password)
+        LoginElements.textboxes.password.invoke('val',password)
     }
 
     static clickOnLoginButton(){
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,14 @@
+import { LoginMethods } from "./pages/login/login.methods"
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    const usuario = 'random01'
+    const contrasena = 'random01'
+    cy.visit('https://demoblaze.com/')
+    cy.get('a[data-target="#logInModal"]').click()
+    LoginMethods.login(usuario, contrasena)
+    cy.get('a#nameofuser').should('contain.text', usuario)
+    cy.wait(5000)
+
   })
 })
