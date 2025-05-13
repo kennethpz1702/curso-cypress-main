79a5ece02343828eddae6d1372fbf9d36336bb97
kennethpz1702 <kennethpz1702@gmail.com>
2025-05-13 09:18:41 -0600

    Creacion de la página Place Order

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,12 @@
+export class PlaceOrderData{
+    static get testData(){
+        return{
+            name: 'Kenneth',
+            country : 'Costa Rica',
+            city : 'San José',
+            creditCard : '6011000000000012',
+            month : '12',
+            year : 2025
+        }
+    }
+}
